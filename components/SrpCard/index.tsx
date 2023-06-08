7801e552132e0d6a2850fce0391a00188d49379a
Andrew Hepworth <hepworta@oregonstate.edu>
2023-06-08 13:12:37 -0700

    animation test

--- components/SrpCard/index.tsx
+++ components/SrpCard/index.tsx
@@ -1,11 +1,20 @@
+"use client";
 /* eslint-disable @next/next/no-img-element */
+import { useRef } from "react";
 import styles from "./SrpCard.module.scss";
 import Link from "next/link";
 
 export default function SrpCard({ vehicle }: { vehicle: any }) {
+  const imgRef = useRef<HTMLImageElement>(null);
   return (
-    <Link className={styles.card} href={`/vehicle/${vehicle.id}`}>
-      <img src={vehicle.image} alt="car" />
+    <Link
+      className={styles.card}
+      href={`/vehicle/${vehicle.id}`}
+      onClick={() => {
+        console.log(imgRef.current?.getBoundingClientRect());
+      }}
+    >
+      <img src={vehicle.image} alt="car" ref={imgRef} />
       <h2>{vehicle.title}</h2>
       <p className={styles.price}>${vehicle.price}</p>
       <p className={styles.vin}>{vehicle.vin}</p>
